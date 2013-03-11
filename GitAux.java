fe08eefb3fbe85bf22f61e5ff94e66e6997ac8c2
Manuel Lopez Rebora <061914677x@alu.uma.es>
2013-03-11 13:19:55 +0100

    modificaciones alumno 2

--- GitAux.java
+++ GitAux.java
@@ -4,21 +4,17 @@
  */
 package prgitejemplo;
 
-/**
- *
- * @author bertoa
- */
 public class GitAux {
-    
-    public void metodoAlumno1(){
-        System.out.println("metodo 1");
-    }
-    
-    public void metodoAlumno2(){
-        System.out.println("metodo 2");
-    }
-    
-    public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
-    }
-}
+	public void metodoAlumno1(){
+		System.out.println("metodo 1");
+		System.out.println("modificado por alum 1"); // insertado por alum1
+	}
+	public void metodoAlumno2(){
+		System.out.println("metodo 2");
+	}
+	public void metodoComunitario(){
+		// Comentario incluido para pruebas
+		System.out.println("Aqui escribimos todos");
+		// modificacaciones alumno 1
+		System.out.println("Alumno 1 inserta esta sentencia");
+	}
